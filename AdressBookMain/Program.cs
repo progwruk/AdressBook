7ca8c5ac5346244345b9557ad6ea3e3afa07d8e1
progwruk <wmeshram@gmail.com>
2022-09-02 07:46:11 +0530

    [Wrukshali] Add: Welcome to Address book program

---
+++ AdressBookMain/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AddressBookMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+        }
+    }
+}
