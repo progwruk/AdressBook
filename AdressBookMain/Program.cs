a228b7782cda0a1f33df8ae1b9272572dddba99a
progwruk <wmeshram@gmail.com>
2022-09-03 07:36:57 +0530

    [Wrukshali] Add: Contact details

--- AdressBookMain/Program.cs
+++ AdressBookMain/Program.cs
@@ -1,11 +1,22 @@
 ﻿using System;
-namespace AddressBookMain
+namespace AdressBookMain
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            Contact contact = new Contact()
+            {
+                FirstName="Wrukshali",
+                LastName="Meshram",
+                Address="Bhadrawati",
+                City="Chandrapur",
+                Zip=442501,
+                PhoneNumber="+910485458907",
+                Email="abc@gmail.com"
+            };
+            Console.WriteLine("FirstName: "+contact.FirstName+"\n"+ "LastName: "+contact.LastName+"\n" +"Address: "+contact.Address+"\n"+"City: "+contact.City+"\n"+"Zip: "+contact.Zip+"\n"+"PhoneNumber: "+contact.PhoneNumber+"\n"+"Email: "+contact.Email);
         }
     }
 }
